7a8c597358da77893fb21b24d480cd877632ff58
Jimmy Sorza <jimmysorza@gmail.com>
2019-07-30 12:23:53 -0500

    implement resolver function mutation

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-// 1
 let links = [{
     id: 'link-0',
     url: 'www.howtographql.com',
     description: 'Fullstack tutorial for GraphQL'
 }]
 
-//2
+let idCount = links.length
 
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
         feed: () => links,
     },
-    Link: {
-        id: (parent) => parent.id,
-        description: (parent) => parent.description,
-        url: (parent) => parent.url,
-    }
+   Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${idCount}`,
+                description: args.description,
+                url: args.url,
+            }
+            links.push(link)
+            return link
+        }
+   }
 }
-
-//3
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',

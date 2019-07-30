fb11fac75f6ca59ee031ea407953fb6cf1df51a1
Jimmy Sorza <jimmysorza@gmail.com>
2019-07-30 11:55:14 -0500

    Extend the schema definition

--- src/index.js
+++ src/index.js
@@ -1,23 +1,5 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-//1
-const typeDefs =`
-    type Query {
-    info: String!
-    feed: [Link!]!  
-}
-
-    type Mutation {
-        post(url: String!, description: String!): Link!
-    }
-
-    type Link {
-        id: ID!
-        description: String!
-        url: String!
-    }
-`
-
 // 1
 let links = [{
     id: 'link-0',
@@ -42,7 +24,7 @@ const resolvers = {
 //3
 
 const server = new GraphQLServer({
-    typeDefs,
+    typeDefs: './src/schema.graphql',
     resolvers
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
